586dc4ab44e370c6a834109ace0cae5cab2c1e68
alexander avila vilchez <aavilav5@hotmail.com>
2014-01-28 22:30:57 -0500

    up

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="css/core.css" type="text/css">
+        <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+        <link href="css/bootstrap.css" rel="stylesheet" media="screen">
+        <link href="css/bootstrap-responsive.css" rel="stylesheet" media="screen">
+        <link href="css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <table class="tables" align="center">
+            <tr>
+                <td><a><img src="img/operaciones.jpg" alt="..." class="img-circle"><br><center>operaciones</center></a></td>
+                <td><a><img src="img/administracion.jpg" alt="..." class="img-circle"><br><center>administracion</center></a></td>
+            </tr><tr>
+                <td><a><img src="img/logistica.jpg" alt="..." class="img-circle"><br><center>logistica</center></a></td>
+                <td><a><img src="img/marketing.jpg" alt="..." class="img-circle"><br><center>marketing</center></a></td>
+            </tr>
+        </table>
+    </body>
+</html>
--- valida.php
+++ valida.php
@@ -1,15 +1,30 @@
 <?php
  session_start();
 $con=mysql_connect('localhost','root','root') or die('Error en el servidor');
-mysql_select_db('control2',$con)or die('Error en la base de datos');
+mysql_select_db('bam',$con)or die('Error en la base de datos');
 $usu=$_POST['usuario'];
 $cla=$_POST['clave'];
-$consulta="select * from usuario where usu_nombre='$usu' and usu_clave='$cla'";
+$mod=$_POST['modulo'];
+$consulta="select * from usuario where usu_nombre='$usu' and usu_clave='$cla' and mod_codigo=$mod";
 $resultado=  mysql_query($consulta);
 $num_regsitros=  mysql_num_rows($resultado);
 if($num_regsitros==1){
-    $_SESSION['auth']='ok';
+    if($mod==1){
+        $_SESSION['1']='ok';
+        header('location:operaciones/index.php');
+    }else if ($mod==2){
+        $_SESSION['2']='ok';
+        header('location:administracion/index.php');
+    }else if ($mod==3){
+        $_SESSION['3']='ok';
+        header('location:logistica/index.php');
+    }else if ($mod==4){
+        $_SESSION['4']='ok';
+        header('location:marketing/index.php');
+    }else if ($mod==5){
+        $_SESSION['5']='ok';
         header('location:index.php');
+    } 
 }
 else   {
     $_SESSION['msj']='error';
