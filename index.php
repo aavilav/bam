48c99ae47b8af7a6c12d61e3fa43d152934bf050
alexander avila vilchez <aavilav5@hotmail.com>
2014-01-28 23:00:49 -0500

    up

--- index.php
+++ index.php
@@ -10,13 +10,44 @@
         <title></title>
     </head>
     <body>
+        <div id="header"><font face="Arial" size="+2" color="black">
+        <center>
+            <h1>
+                <marquee bgcolor="#FFFFFF" width="100%" scrolldelay="100" scrollamount="5" direction="left" loop="infinite">
+<!--           EMPRESA         -->  CONSORCIO IMPORT SERVICE GROUP
+                </marquee>
+            </h1>
+        </center>
+    </font>
+</div>
+        <div class="navbar navbar-inverse">
+      <div class="navbar-inner">
+        <div class="container">
+          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="brand" href="index.php">BAM - ERP</a>
+          <div class="nav-collapse collapse">
+            <ul class="nav">
+              <li><a href="operaciones/index.php">Operaciones</a></li>
+              <li><a href="administracion/index.php">Administraci&oacute;n</a></li>
+              <li><a href="logistica/index.php">Logistica</a></li>
+              <li><a href="marketing/index.php">Marketing</a></li>
+            </ul>
+              <p class="navbar-text pull-right"><a href="cerrar.php" class="navbar-link">Cerrar Sesion</a></p>         
+          </div>
+        </div>
+      </div>
+    </div>
         <table class="tables" align="center">
             <tr>
-                <td><a><img src="img/operaciones.jpg" alt="..." class="img-circle"><br><center>operaciones</center></a></td>
-                <td><a><img src="img/administracion.jpg" alt="..." class="img-circle"><br><center>administracion</center></a></td>
+                <td><a href="operaciones/index.php"><img src="img/operaciones.jpg" alt="..." class="img-circle"><br><center><h3>Operaciones</h3></center></a></td>
+                <td><a href="administracion/index.php"><img src="img/administracion.jpg" alt="..." class="img-circle"><br><center><h3>Administracion</h3></center></a></td>
             </tr><tr>
-                <td><a><img src="img/logistica.jpg" alt="..." class="img-circle"><br><center>logistica</center></a></td>
-                <td><a><img src="img/marketing.jpg" alt="..." class="img-circle"><br><center>marketing</center></a></td>
+                <td><a href="logistica/index.php"><img src="img/logistica.jpg" alt="..." class="img-circle"><br><center><h3>Logistica</h3></center></a></td>
+                <td><a href="marketing/index.php"><img src="img/marketing.jpg" alt="..." class="img-circle"><br><center><h3>Marketing</h3></center></a></td>
             </tr>
         </table>
     </body>
